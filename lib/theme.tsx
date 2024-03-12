1e85681c67be62753cb4be529f6316f2c457ab7d
Jake Shilling <shilling.jake@gmail.com>
2024-03-12 14:47:23 -0400

    Set font and favicon

---
+++ lib/theme.tsx
@@ -0,0 +1,12 @@
+import { Montserrat } from 'next/font/google';
+
+export const MontserratBlack = Montserrat({
+    subsets: ['latin'],
+    weight: "900",
+});
+
+export const MontserratRegular = Montserrat({
+    subsets: ['latin'],
+    weight: "400",
+});
+
