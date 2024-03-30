61b20aa1403288fb211d8fca95da7f2f3762ad19
Jake Shilling <shilling.jake@gmail.com>
2024-03-29 21:18:23 -0400

    Create a rough outline of the homepage

--- lib/Links.tsx
+++ lib/Links.tsx
@@ -1,6 +1,6 @@
-export const Calendarly = "https://calendly.com/functorfactory/30min"
-export const Github = "https://github.com/FunctorFactory"
-export const Twitter = "https://twitter.com/FunctorFactory"
-export const LinkedIn = "https://www.linkedin.com/company/functor-factory"
-export const YouTube = "https://www.youtube.com/@FunctorFactory"
-export const Email = "mailto:jshilling@functorfactory.com"
+export const Calendarly = 'https://calendly.com/functorfactory/consultation';
+export const Github = 'https://github.com/FunctorFactory';
+export const Twitter = 'https://twitter.com/FunctorFactory';
+export const LinkedIn = 'https://www.linkedin.com/company/functor-factory';
+export const YouTube = 'https://www.youtube.com/@FunctorFactory';
+export const Email = 'mailto:jshilling@functorfactory.com';
